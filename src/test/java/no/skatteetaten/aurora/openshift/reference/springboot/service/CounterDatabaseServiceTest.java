a5bef1ab2d90b5bfedc0aad7937030e0d8c2c780
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2021-06-18 13:00:50 +0200

    tests converted from spock to junit 5

---
+++ src/test/java/no/skatteetaten/aurora/openshift/reference/springboot/service/CounterDatabaseServiceTest.java
@@ -0,0 +1,30 @@
+package no.skatteetaten.aurora.openshift.reference.springboot.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+
+@JdbcTest
+@AutoConfigureEmbeddedDatabase
+class CounterDatabaseServiceTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    @DisplayName("Verify maintains counter")
+    void verifyCounter() {
+        CounterDatabaseService service = new CounterDatabaseService(jdbcTemplate);
+        Long counter1 = service.getAndIncrementCounter();
+        Long counter2 = service.getAndIncrementCounter();
+
+        assertEquals(1, counter1);
+        assertEquals(2, counter2);
+    }
+}
